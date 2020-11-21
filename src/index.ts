db4f7bb127338854cdad8917b6ef2b9a4565e0b5
Jon <bangarangler@gmail.com>
2020-11-21 17:14:02 -0500

    added register and login.
    having trouble with cookie in insomnia, playground etc.
    
    have to come back to it later.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import "reflect-metadata";
 import * as dotenv from "dotenv";
 dotenv.config();
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import microConfig from "./mikro-orm.config";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import redis from "redis";
 import express from "express";
+import { MikroORM } from "@mikro-orm/core";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { __prod__ } from "./constants";
+import { MyContext } from "./types";
+import microConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -18,12 +22,36 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: false,
+      secret: process.env.REDIS_SECRET!, // add ! must be in .env
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+    playground: {
+      settings: {
+        "request.credentials": "include",
+      },
+    },
   });
 
   apolloServer.applyMiddleware({ app });
